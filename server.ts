93daa9df237d81feb0197f2c9c7619392f43c802
Rhino <rhinoandre@gmail.com>
2022-11-15 17:19:56 -0300

    fix: move database start to start

--- server.ts
+++ server.ts
@@ -1,23 +1,15 @@
 import Nullstack, { NullstackServerContext } from 'nullstack';
 import Application from './src/Application';
-import { MongoClient, Db } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-interface CustomNullstackServerContext extends NullstackServerContext {
-  database: Db
-}
+const context = Nullstack.start(Application) as NullstackServerContext;
 
-const context = Nullstack.start(Application) as CustomNullstackServerContext;
-
-async function startDatabase() {
+context.start = async function start() {
   const { secrets } = context;
 
   const databaseClient = new MongoClient(`${secrets.databaseHost}`);
   await databaseClient.connect();
-  context.database = await databaseClient.db(`${secrets.databaseName}`);
-}
-
-context.start = async function start() {
-  await startDatabase();
+  context['database'] = await databaseClient.db(`${secrets.databaseName}`);
 }
 
 export default context;
